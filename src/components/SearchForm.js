4a207bfe5178e049c1ec41cbfc1cb732817d11b5
Sean Birmingham <seannbirmingham98@gmail.com>
2019-12-13 22:29:37 -0500

    added searchTerm useState and handleChange function

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function SearchForm() {
 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value)
+  };
+
+  console.log("search term", searchTerm)
   return (
     <section className="search-form">
       <form>
         <label htmlFor="name"></label>
-        <input id="name" type="text" name="textfield" placeholder="search" />
+        <input id="name" type="text" name="textfield" placeholder="search" onChange={handleChange} value={searchTerm} />
       </form>
     </section>
   );
